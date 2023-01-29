0674b19c8b22679408420793096d83df5acada37
Suhyeon AHN <suahn@e1r3p12.clusters.42paris.fr>
2023-01-29 19:08:24 +0100

    ex01 done

--- js00/ex01/helloSomeone.js
+++ js00/ex01/helloSomeone.js
@@ -1,20 +1,20 @@
-function helloSomeone(p) {
-    if (typeof p === "string" ) {
+export default function helloSomeone(p) {
+    if (typeof p === "string") {
         if (p === "")
-            ;
-    }
-    if (typeof p === "number" ) {
-        if (isNan(p))
-            ;
-        if (p <= 0)
-            ;
+            console.log("Who are you?");
         else
-            ;
+            console.log(`Hello ${p}`);
     }
-    if (p === null ) // Null
-        ;
-    if (typeof p === "undefined" )
-        ;
+    if (typeof p === "number") {
+        if (isNaN(p))
+            console.log("Age is just a number");
+        if (p <= 0)
+            console.log("I am Benjamin Button!");
+        else
+            console.log(`My age is ${p}`);
+    }
+    if (p === null) // Null
+        console.log("I am null and void");
+    if (typeof p === "undefined")
+        console.log("Nobody can define me!");
 }
-
-const obj = JSON.parse(text);
