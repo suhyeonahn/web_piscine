8a41b0b162a06cbacae9ca931292a8a696a90879
suhyeonahn <fongdang01@gmail.com>
2023-02-05 18:47:19 +0100

    ex03 done

---
+++ js01/ex03/ex03.js
@@ -0,0 +1,16 @@
+const lis = document.getElementsByTagName("li");
+// nodelist to array
+Array.from(lis).forEach(item => {
+    item.addEventListener("mouseover", (event) => {
+        event.target.style.backgroundColor = "black";
+        event.target.style.color = "cornflowerblue";
+    })
+    // mouseleave - no bubbling
+    item.addEventListener("mouseleave", (event) => {
+        event.target.style.backgroundColor = "cornflowerblue";
+        event.target.style.color = "black";
+    })
+    item.addEventListener("click", (event) => {
+        alert(`You've select ${event.target.innerHTML}.`);
+    })
+})
