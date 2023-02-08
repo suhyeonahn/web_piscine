aaa0303d6b1e777cd311bb7dc22d147ae0ffacc1
Suhyeon AHN <suahn@e2r2p10.clusters.42paris.fr>
2023-02-08 18:27:54 +0100

    js02-ex03 ok

---
+++ js02/ex03/spaghetti.js
@@ -0,0 +1,31 @@
+const level = ["boilng pasta..", "blanching brocolis..", "preparing garlics and onions..", "frying bacons", "sauting the sauce and vegetables", "mixing all ingredients together"];
+const interval = [10000, 1000, 2000, 2000, 3000, 3000];
+let count = 0;
+
+function bake(i) {
+    let promise = new Promise((resolve, reject) => {
+        if (Math.random() >= 0.2) {
+            setTimeout(() => {
+                if (count == 5)
+                    resolve(level[i] + "\nSUCCESS!\nBon appetit :)");
+                else
+                    resolve(level[i] + "\nSUCCESS!");
+            }, interval[i])
+        } else {
+            reject(level[i] + "\n" + "FAIL. * My chef will scold me :( *");
+        }
+    });
+    promise.then((message) => {
+        console.log(message);
+        count++;
+        if(count == 5)
+            bake(5);
+    }).catch((message) => { 
+        console.error(message);
+        bake(i);
+    });
+}
+
+for (let i = 0; i < 5; i++) {
+        bake(i);
+}
