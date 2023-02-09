01fd910d40644f42e0ba3980734f5b8f61805d51
suhyeonahn <fongdang01@gmail.com>
2023-02-09 16:57:14 +0100

    js02 all ok

--- js02/ex05/fetch.js
+++ js02/ex05/fetch.js
@@ -1,16 +1,46 @@
+import fetch from 'node-fetch';
+
 const MAX = 100;
 const MIN = 1;
+const n = [];
 
-const n = Math.floor(Math.random() * (MAX - MIN + 1) + MIN);
+for (let i = 0; i < 5; i++)
+    n.push(Math.floor(Math.random() * (MAX - MIN + 1) + MIN));
 
-fetch('https://jsonplaceholder.typicode.com/posts', {
-    method: 'PATCH',
-    body: JSON.stringify({
-        id: n,
-    }),
-    headers: {
-        'Content-Type': 'application/json; charset=UTF-8',
-    },
-})
-   .then(resonse => resonse.json())
-   .then(json => console.log(json));
+function fetchPost(count) {
+    return fetch(`https://jsonplaceholder.typicode.com/posts/${n[count]}`, {
+            method: 'GET',
+        })
+        .then(resonse => resonse.json())
+        .catch(e => console.error(e));
+}
+
+function fetchComments(count) {
+    return fetch(`https://jsonplaceholder.typicode.com/posts/${n[count]}/comments`, {
+        method: 'GET',
+    })
+    .then(resonse => resonse.json())
+    .catch(e => console.error(e));
+}
+
+function postLog(content, n) {
+    console.log(`POST [${n}]`)
+    console.log("Title: " + content.title);
+    console.log(content.body + "\n");
+}
+
+function commentsLog(content) {
+    console.log("User name: " + content.name);
+    console.log("Email: " + content.email);
+    console.log(content.body + "\n");
+}
+
+async function asyncCall() {
+    for (let i = 0; i < n.length; i++) {
+        let post = await fetchPost(i);
+        postLog(post, i + 1);
+        let comments = await fetchComments(i);
+        comments.forEach(commentsLog);
+    }
+}
+asyncCall();
