4e9061ed88b12f4659d3ef3a797f666292752ced
Suhyeon AHN <suahn@e1r11p9.clusters.42paris.fr>
2023-02-17 23:17:33 +0100

    working on bootstrap

--- fe01/ex02/src/App.js
+++ fe01/ex02/src/App.js
@@ -3,18 +3,39 @@ import Home from './pages/Home';
 import About from './pages/About';
 import Contact from './pages/Contact';
 
+import { Button, Navbar, Container, Row, Col } from 'react-bootstrap';
+
+
 function App() {
       return (
-         <BrowserRouter>
              <div className="App">
-              <Home />
-              <Routes>
-                <Route path="/" element={<Home />} />
-                <Route path="/about" element={<About />} />
-                <Route path="/contact" element={<Contact />} />
-              </Routes>
+      <Container>
+      <Row>
+        <Col xs={12} md={8}>
+          {/* Content goes here */}
+        </Col>
+        <Col xs={12} md={4}>
+          {/* Sidebar goes here */}
+        </Col>
+      </Row>
+    </Container>
+    <Navbar bg="dark" variant="dark">
+  <Navbar.Brand href="#home">My App</Navbar.Brand>
+  <Navbar.Collapse className="justify-content-end">
+    <Navbar.Text>
+      Signed in as: <a href="#login">username</a>
+    </Navbar.Text>
+  </Navbar.Collapse>
+</Navbar>
+<Button variant="primary" size="lg" block>Click me</Button>
+<BrowserRouter>
+      <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+      </Routes>
+</BrowserRouter>
              </div>
-         </BrowserRouter>
      );
  }
 
