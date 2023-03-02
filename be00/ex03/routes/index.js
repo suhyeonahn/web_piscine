064372112eae7f8a0e0cd5bc04605ba9a20f195f
Suhyeon AHN <suahn@e2r10p14.clusters.42paris.fr>
2023-03-02 21:41:33 +0100

    be00-ex03 continue

--- be00/ex03/routes/index.js
+++ be00/ex03/routes/index.js
@@ -1,12 +1,26 @@
 const express = require('express')
+const _ = require('lodash');
 const router = express.Router()
 
 router.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
-router.get('/about', (req, res) => {
-  res.send('About Us')
+router.get('/station/:name', (req, res) => {
+    const stationName = req.params.name;
+    const data = _.readJsonSync('../seoul_metro_station.json');
+    const specificArray = _.find(data, { station_nm: stationName }).array;
+    res.json(specificArray);
 })
+
+router.get('/line', (req, res) => {
+    const lineNumber = req.query.line_num;
+    res.send(`Selected line number: ${lineNumber}`);
+})
+
+router.post('/station/id', (req, res) => {
+    res.send('About Us')
+})
+
 
 module.exports = router
