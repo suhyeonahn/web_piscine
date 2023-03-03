e69be52f5dbd48a33080c0f26fd8d0b43f86fa92
Suhyeon AHN <suahn@e1r10p14.clusters.42paris.fr>
2023-03-03 20:03:16 +0100

    be00-ex03 ok

--- be00/ex03/routes/index.js
+++ be00/ex03/routes/index.js
@@ -12,22 +12,29 @@ router.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
-router.get('/station/:name', (req, res) => {
+router.get('/station/:name', (req, res, next) => {
     const stationName = req.params.name
     const regex = /^[A-Za-z][A-Za-z0-9\s]*$/;
     let result = [];
+
     if (regex.test(stationName))
         result = data.DATA.filter(item => item.station_nm_eng === stationName);
     else
         result = data.DATA.filter(item => item.station_nm === stationName);
+
+    if (result.length == 0)
+        return res.status(400).json({ error: 'station_nm is not correct' });
+
     res.status(200).json(result)
+
 })
 
 router.get('/line', (req, res) => {
     // default is '01호선'
     const lineNum = req.query.line_num || '01호선';
     const result = data.DATA.filter(item => item.line_num === lineNum);
-  res.status(200).json(result);
+
+    res.status(200).json(result);
 });
 
 router.post('/station/id', (req, res) => {
