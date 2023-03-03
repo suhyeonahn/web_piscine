60cd8e54028591f0cfa791c5d4d4c4d6934bb84a
Suhyeon AHN <suahn@e1r10p14.clusters.42paris.fr>
2023-03-03 18:13:28 +0100

    err POST(frCode is empty), need to add error handlers

--- be00/ex03/routes/index.js
+++ be00/ex03/routes/index.js
@@ -1,26 +1,43 @@
 const express = require('express')
+const bodyParser = require('body-parser');
 const _ = require('lodash');
 const router = express.Router()
+
+const data = require('../seoul_metro_station.json')
+
+router.use(bodyParser.json());
 
 router.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
 router.get('/station/:name', (req, res) => {
-    const stationName = req.params.name;
-    const data = _.readJsonSync('../seoul_metro_station.json');
-    const specificArray = _.find(data, { station_nm: stationName }).array;
-    res.json(specificArray);
+    const stationName = req.params.name
+    const regex = /^[A-Za-z][A-Za-z0-9\s]*$/;
+    let result = [];
+    if (regex.test(stationName))
+        result = data.DATA.filter(item => item.station_nm_eng === stationName);
+    else
+        result = data.DATA.filter(item => item.station_nm === stationName);
+    res.status(200).json(result)
 })
 
 router.get('/line', (req, res) => {
-    const lineNumber = req.query.line_num;
-    res.send(`Selected line number: ${lineNumber}`);
-})
+    // default is '01호선'
+    const lineNum = req.query.line_num || '01호선';
+    const result = data.DATA.filter(item => item.line_num === lineNum);
+  res.status(200).json(result);
+});
 
 router.post('/station/id', (req, res) => {
-    res.send('About Us')
-})
+  const frCode = req.body.fr_code;
 
+  console.log(frCode)
+  if (!frCode)
+    return res.status(400).json({ error: 'fr_code is required' });
 
-module.exports = router
+  const result = data.DATA.filter(item => item.fr_code === frCode);
+  res.send(`The value of fr_code is ${frCode}`);
+});
+
+module.exports = router;
